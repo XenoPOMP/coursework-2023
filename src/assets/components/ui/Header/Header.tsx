27c33f5f73359f89622b732445574a1558bf60db
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-03-27 00:35:17 +0300

    настроил сетку в хедере

--- src/assets/components/ui/Header/Header.tsx
+++ src/assets/components/ui/Header/Header.tsx
@@ -8,22 +8,28 @@ import Navbar from '@ui/Navbar/Navbar';
 const Header: FC<HeaderProps> = ({ tabIndex }) => {
   return (
     <header className={cn(styles.appHeader)}>
-      <Navbar tabIndex={tabIndex} />
+      <div className={cn(styles.container, styles.left)}>
+        <Navbar tabIndex={tabIndex} />
+      </div>
 
-      <Logotype variant={'monochrome'} />
+      <div className={cn(styles.container, styles.center)}>
+        <Logotype variant={'monochrome'} />
+      </div>
 
-      <div className={cn(styles.squareButton)}>
-        <svg
-          width='31'
-          height='15'
-          viewBox='0 0 31 15'
-          fill='none'
-          xmlns='http://www.w3.org/2000/svg'
-        >
-          <rect y='6' width='30' height='3' rx='1.5' />
-          <rect x='13' width='17.1429' height='3' rx='1.5' />
-          <rect y='12' width='17.1429' height='3' rx='1.5' />
-        </svg>
+      <div className={cn(styles.container, styles.right)}>
+        <div className={cn(styles.squareButton)}>
+          <svg
+            width='31'
+            height='15'
+            viewBox='0 0 31 15'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <rect y='6' width='30' height='3' rx='1.5' />
+            <rect x='13' width='17.1429' height='3' rx='1.5' />
+            <rect y='12' width='17.1429' height='3' rx='1.5' />
+          </svg>
+        </div>
       </div>
     </header>
   );
