6a781d632d4a3f5bcf015dec31b29f37c45d9c08
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-03-26 23:55:58 +0300

    добавил кнопку Меню

--- src/assets/components/ui/Header/Header.tsx
+++ src/assets/components/ui/Header/Header.tsx
@@ -8,6 +8,20 @@ const Header: FC<HeaderProps> = ({}) => {
   return (
     <header className={cn(styles.appHeader)}>
       <Logotype variant={'monochrome'} />
+
+      <div className={cn(styles.squareButton)}>
+        <svg
+          width='31'
+          height='15'
+          viewBox='0 0 31 15'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <rect y='6' width='30' height='3' rx='1.5' />
+          <rect x='13' width='17.1429' height='3' rx='1.5' />
+          <rect y='12' width='17.1429' height='3' rx='1.5' />
+        </svg>
+      </div>
     </header>
   );
 };
