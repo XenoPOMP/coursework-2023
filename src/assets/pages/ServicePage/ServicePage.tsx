db296abcff09697dae127ed448d1f95c8d30af82
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-03-27 22:21:49 +0300

    add useSearchParams hook

--- src/assets/pages/ServicePage/ServicePage.tsx
+++ src/assets/pages/ServicePage/ServicePage.tsx
@@ -4,9 +4,11 @@ import styles from './ServicePage.module.scss';
 import { ServicePageProps } from './ServicePage.props';
 import Page from '@components/Page/Page';
 import useLocalization from '@hooks/useLocalization';
+import { useSearchParams } from 'react-router-dom';
 
 const ServicePage: FC<ServicePageProps> = ({}) => {
   const loc = useLocalization();
+  const [params, setParams] = useSearchParams();
 
   return (
     <Page
