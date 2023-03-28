ea74461cef4879cbb346e8064de17f866f721f1f
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-03-29 01:59:57 +0300

    change theme title to light

--- src/assets/providers/ThemeProvider/ThemeProvider.tsx
+++ src/assets/providers/ThemeProvider/ThemeProvider.tsx
@@ -2,9 +2,16 @@ import { FC } from 'react';
 import { ProviderProps } from '@providers/Provider.props';
 import cn from 'classnames';
 import styles from './ThemeProvider.module.scss';
+import useAppSettings from '@hooks/useAppSettings';
 
 const ThemeProvider: FC<ProviderProps> = ({ children }) => {
-  document.body.className = cn(styles.themes, styles.dark);
+  const { theme } = useAppSettings();
+
+  document.body.className = cn(
+    styles.themes,
+    theme.get() === 'light' ? styles.light : '',
+    theme.get() === 'dark' ? styles.dark : '',
+  );
 
   return <>{children}</>;
 };
