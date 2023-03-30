c7cfaf765ff4786f6ff5c3845edb5d6f317bdd02
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-03-30 15:42:58 +0300

    update ThemeProvider test

--- src/__tests__/ThemeProvider.test.tsx
+++ src/__tests__/ThemeProvider.test.tsx
@@ -4,6 +4,9 @@ import cn from 'classnames';
 import styles from '@providers/ThemeProvider/ThemeProvider.module.scss';
 import skipTestCondition from '@utils/skipTestCondition';
 import renderWithProviders from '@utils/renderWithProviders';
+import { screen } from '@testing-library/react';
+
+const clc = require('cli-colors');
 
 describe.skipIf(skipTestCondition('FRONTEND'))('Theme Provider', () => {
   test('Classname', () => {
@@ -11,14 +14,19 @@ describe.skipIf(skipTestCondition('FRONTEND'))('Theme Provider', () => {
       useRedux: true,
     });
 
-    const bodyClasses = document.body.classList.contains(styles.themes);
     const themeClassesDefined =
-      document.body.classList.contains(styles.dark) ||
-      document.body.classList.contains(styles.light);
+      document.body.classList.contains(styles.themes) &&
+      (document.body.classList.contains(styles.dark) ||
+        document.body.classList.contains(styles.light));
 
-    console.log(document.body);
+    console.log(
+      `${clc.green(`[DEBUG]`)} Body class list contains themes: ${
+        themeClassesDefined
+          ? clc.green(`${themeClassesDefined}`)
+          : clc.red(`${themeClassesDefined}`)
+      }`,
+    );
 
-    expect(bodyClasses).toBe(true);
     expect(themeClassesDefined).toBe(true);
   });
 });
