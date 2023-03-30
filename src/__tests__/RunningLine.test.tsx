df722c2a2637502ecc7d6c0d1b9dab0135eee6fc
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-03-30 16:05:25 +0300

    add test for vertical alignment for RunningLine

--- src/__tests__/RunningLine.test.tsx
+++ src/__tests__/RunningLine.test.tsx
@@ -2,6 +2,7 @@ import { describe, expect, test } from 'vitest';
 import skipTestCondition from '@utils/skipTestCondition';
 import renderWithProviders, { RenderOptions } from '@utils/renderWithProviders';
 import RunningLine from '@ui/RunningLine/RunningLine';
+import styles from '@ui/RunningLine/RunningLine.module.scss';
 
 describe.skipIf(skipTestCondition('FRONTEND'))('Running line', () => {
   test('Match snapshot', () => {
@@ -24,5 +25,14 @@ describe.skipIf(skipTestCondition('FRONTEND'))('Running line', () => {
 
     expect(element).toBeDefined();
     expect(element?.getAttribute('scrolldelay')).toBeDefined();
+  });
+
+  test('Vertical align', () => {
+    renderWithProviders(<RunningLine scrollDelay={65} verticalAlign />);
+
+    const element = document.querySelector('marquee');
+
+    expect(element).toBeDefined();
+    expect(element?.classList.contains(styles.vAlign)).toBe(true);
   });
 });
