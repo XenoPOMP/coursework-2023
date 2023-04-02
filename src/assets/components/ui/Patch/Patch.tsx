9706edab31a47ffef7b4a5fa12ec4cc32d626fab
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-02 18:41:18 +0300

    add mobile view for patch

--- src/assets/components/ui/Patch/Patch.tsx
+++ src/assets/components/ui/Patch/Patch.tsx
@@ -4,25 +4,42 @@ import styles from './Patch.module.scss';
 import { PatchProps } from './Patch.props';
 import RunningLine from '@ui/RunningLine/RunningLine';
 import numericGenerator from '@utils/numericGenerator';
+import { MobileView, BrowserView } from 'react-device-detect';
 
 const Patch: FC<PatchProps> = ({}) => {
   const scrollSpeed = 8;
 
   return (
-    <RunningLine
-      direction={'up'}
-      className={cn(styles.patch)}
-      scrollSpeed={scrollSpeed}
-      scrollDelay={0}
-    >
-      <div className={cn(styles.content)}>
-        {numericGenerator(70).map((key) => (
-          <span className={cn(styles.word)} key={`patch-element-${key}`}>
-            smartace{' '}
-          </span>
-        ))}
-      </div>
-    </RunningLine>
+    <>
+      <BrowserView>
+        <RunningLine
+          direction={'up'}
+          className={cn(styles.patch)}
+          scrollSpeed={scrollSpeed}
+          scrollDelay={0}
+        >
+          <div className={cn(styles.content)}>
+            {numericGenerator(70).map((key) => (
+              <span className={cn(styles.word)} key={`patch-element-${key}`}>
+                smartace{' '}
+              </span>
+            ))}
+          </div>
+        </RunningLine>
+      </BrowserView>
+
+      <MobileView>
+        <div className={cn(styles.patch)}>
+          <div className={cn(styles.content)}>
+            {numericGenerator(70).map((key) => (
+              <span className={cn(styles.word)} key={`patch-element-${key}`}>
+                smartace{' '}
+              </span>
+            ))}
+          </div>
+        </div>
+      </MobileView>
+    </>
   );
 };
 
