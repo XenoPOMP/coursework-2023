556c8dffef3ab2bb0c4ce96fc41945d06027952c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-03 20:19:47 +0300

    create new adaptive
    Created new adaptive for settings page

--- src/assets/redux/reducers/appSettingsSlice.ts
+++ src/assets/redux/reducers/appSettingsSlice.ts
@@ -14,7 +14,7 @@ export type AppSettings = {
 };
 
 const initialState: AppSettings = {
-  appVersion: 'beta 0.5.2',
+  appVersion: 'beta 0.6',
   language: 'ru',
   cookiePrefix: 'smart-ace',
   theme: 'light',
