a3d576bf97eb86cff2a0b9544bffdd8ae8ca97e8
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-03 23:39:43 +0300

    write test for Header

---
+++ src/__tests__/Header.test.tsx
@@ -0,0 +1,45 @@
+import { describe, expect, test, vi } from 'vitest';
+import skipTestCondition from '@utils/skipTestCondition';
+import renderWithProviders, { RenderOptions } from '@utils/renderWithProviders';
+import Header from '@ui/Header/Header';
+import styles from '@ui/Header/Header.module.scss';
+import { fireEvent } from '@testing-library/react';
+import { useNavigate } from 'react-router-dom';
+import { NavigateFunction } from 'react-router-dom';
+
+describe.skipIf(skipTestCondition('FRONTEND'))('Header component', () => {
+  const defaultOptions: RenderOptions = {
+    useRedux: true,
+    useRouter: true,
+  };
+
+  test('Match snapshot', () => {
+    expect(
+      renderWithProviders(<Header />, {
+        ...defaultOptions,
+      }),
+    ).toMatchSnapshot();
+  });
+
+  test('Error handling', () => {
+    expect(() =>
+      renderWithProviders(<Header renderNav renderBackButton />, {
+        ...defaultOptions,
+      }),
+    ).toThrowError(Error);
+  });
+
+  test('Back button navigate', () => {
+    renderWithProviders(<Header renderNav={false} renderBackButton={true} />, {
+      ...defaultOptions,
+    });
+
+    const backButton = document.querySelectorAll(
+      `div.${styles.squareButton}`,
+    )[0];
+
+    fireEvent.click(backButton);
+
+    expect(backButton).toBeDefined();
+  });
+});
