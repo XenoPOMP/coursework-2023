e121fad5208ee922def6ee2e57abd44bbc7e8bad
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-08 14:01:16 +0300

    add TODO lines

--- src/assets/contexts/WebSocketContext.tsx
+++ src/assets/contexts/WebSocketContext.tsx
@@ -2,6 +2,8 @@ import useEnv from '@hooks/useEnv';
 import { createContext } from 'react';
 import { io, Socket } from 'socket.io-client';
 
+// TODO add parameter "device" to socket
+// TODO add device type analysis
 const { DOMAIN } = useEnv();
 export const socket = (allowed: boolean, sessionToken: string) =>
   io(`ws://${DOMAIN}:80?allow=${allowed}&sessionToken=${sessionToken}`);
