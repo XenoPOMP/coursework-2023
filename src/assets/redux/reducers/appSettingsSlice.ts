c4b638273745495340d60fbd1c4d1f737f0a797e
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-11 17:15:36 +0300

    change app version

--- src/assets/redux/reducers/appSettingsSlice.ts
+++ src/assets/redux/reducers/appSettingsSlice.ts
@@ -14,7 +14,7 @@ export type AppSettings = {
 };
 
 const initialState: AppSettings = {
-  appVersion: '1.1',
+  appVersion: '1.1.1',
   language: 'ru',
   cookiePrefix: 'smart-ace',
   theme: 'light',
