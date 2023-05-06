24b948e14a24605d07aeb8154f574527f21e8dbe
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-05-06 13:47:55 +0300

    add max page width

--- src/assets/components/Layout/Layout.tsx
+++ src/assets/components/Layout/Layout.tsx
@@ -3,13 +3,15 @@ import { LayoutProps } from './Layout.props';
 import GlobalProvider from '@providers/GlobalProvider/GlobalProvider';
 import Header from '@ui/Header/Header';
 import AnalyticsAgreementMessage from '@ui/AnalyticsAgreementMessage/AnalyticsAgreementMessage';
+import styles from './Layout.module.scss';
+import cn from 'classnames';
 
 const Layout: FC<LayoutProps> = ({ children, header }) => {
   return (
     <GlobalProvider>
       <Header {...header} />
 
-      <main>{children}</main>
+      <main className={cn(styles.appMain)}>{children}</main>
 
       <AnalyticsAgreementMessage />
     </GlobalProvider>
