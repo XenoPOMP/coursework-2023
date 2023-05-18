8fd5a80667826fe26d78af5c01327e960a97f6ce
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-05-18 19:34:31 +0300

    update docs

--- src/assets/hooks/useLocalStorage.ts
+++ src/assets/hooks/useLocalStorage.ts
@@ -1,9 +1,19 @@
 type UseLocalStorageType<T> = [getItem: T, setItem: (newValue: T) => void];
 
+/**
+ * useLocalStorage hook.
+ *
+ * @example
+ * const [getItem, setItem] = useLocalStorage<boolean>('some-item', false);
+ *
+ * @param name								 name of local storage item.
+ * @param [defaultItem]        default item value.
+ */
 export const useLocalStorage = <T>(
 	name: string,
 	defaultItem?: T
 ): UseLocalStorageType<T> => {
+	// Item from local storage
 	const getItem = localStorage.getItem(name);
 
 	// Check if state is not created
