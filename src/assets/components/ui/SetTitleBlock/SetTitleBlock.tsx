5ec49b8a9c74d25569c7fbffc418a32e04c3618c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-05-18 19:43:56 +0300

    update comments

--- src/assets/components/ui/SetTitleBlock/SetTitleBlock.tsx
+++ src/assets/components/ui/SetTitleBlock/SetTitleBlock.tsx
@@ -1,10 +1,19 @@
 import cn from 'classnames';
 import { FC } from 'react';
 
+import { SettingsTabTitleLocale } from '@localization/Localization';
+
 import styles from './SetTitleBlock.module.scss';
 import { SetTitleBlockProps } from './SetTitleBlock.props';
 
+/**
+ * Settings page title block.
+ *
+ * @param {SettingsTabTitleLocale} locales         block locales of type {@link SettingsTabTitleLocale}.
+ * @constructor
+ */
 const SetTitleBlock: FC<SetTitleBlockProps> = ({ locales }) => {
+	// Get locales from argument
 	const { title, desc } = locales;
 
 	return (
